15739cb328fdb930fe88188b87a5f770bd0d40f0
Agniprobho Mazumder! <agniprobho@k-state.edu>
2018-09-10 17:27:37 -0500

    Shell script to convert temperature from degrees Fahrenheit to degrees Celcius

---
+++ temperature.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+read -p 'Enter temperature in Fahrenheit: ' tt
+
+if [ -z "$tt" ]
+then
+	echo "A temperature input is required"
+	exit 
+fi
+
+bc <<_EOF_
+scale=6
+
+#first we define the function
+define temp_conv(x)
+{
+   t=(x-32)*5/9
+   return t
+}
+
+
+# then use the function to do the calculation
+x=$tt
+print "Temperature in degrees Celcius is ",temp_conv(x),"\n"
+
+_EOF_
